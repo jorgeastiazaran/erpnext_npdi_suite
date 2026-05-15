68bb21600731d51f0792eaefc5e5bfe3e99d9c9c
Jorge Astiazaran <jorgeastiazaran@Jorges-Air.lan>
2026-05-15 15:29:05 -0600

    refactor: restructure app and formalize project/task field injection

--- erpnext_npdi_suite/hooks.py
+++ erpnext_npdi_suite/hooks.py
@@ -10,16 +10,16 @@ app_email = "tecnofoodmx@gmail.com"
 app_license = "MIT"
 
 # Hook disparado al instalar la app en una instancia
-after_install = "erpnext_npdi_suite.setup.install.after_install"
+after_install = "erpnext_npdi_suite.erpnext_npdi_suite.setup.install.after_install"
 
 # Ganchos de documentos transaccionales
 doc_events = {
     "Task": {
         # Dispara el recálculo CPM de todo el proyecto en segundo plano al actualizar una tarea
-        "on_update": ["erpnext_npdi_suite.engine.cpm.on_task_update"]
+        "on_update": ["erpnext_npdi_suite.erpnext_npdi_suite.engine.cpm.on_task_update"]
     },
     "Project": {
         # Transfiere los atributos extendidos desde Project Template Task hacia las Tareas generadas
-        "after_insert": ["erpnext_npdi_suite.engine.cpm.on_project_insert"]
+        "after_insert": ["erpnext_npdi_suite.erpnext_npdi_suite.engine.cpm.on_project_insert"]
     }
 }
