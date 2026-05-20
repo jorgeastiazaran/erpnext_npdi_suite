a5640b2e21fcdc5d24abe029cdaac0d2d3d6d528
Jorge Astiazaran <jorgeastiazaran@Jorges-Air.lan>
2026-05-20 16:17:37 -0600

    style: fix transparent overlapping on fullscreen mode by defining solid background variables and increasing z-index

--- frontend/src/GanttView.tsx
+++ frontend/src/GanttView.tsx
@@ -352,7 +352,7 @@ export default function GanttView({
           left: 0,
           right: 0,
           bottom: 0,
-          zIndex: 9999,
+          zIndex: 99999,
           display: 'flex',
           flexDirection: 'column'
         } : {})
